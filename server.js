3b4c9b0eb204fbaaa58922ffb3c91fa8372dcea0
Emmet Quint <emmetmquint@gmail.com>
2023-03-08 21:43:51 -0600

    fixed style issues

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const newFolderName = function (folders) {
   let count = 1
-  
   if (!folders.includes('New Folder')) {
-      return 'New Folder'
-    } 
-
-  for (i = 0; i < folders.length + 1; i++) {
+    return 'New Folder'
+  }
+  for (let i = 0; i < folders.length + 1; i++) {
     if (folders.includes(`New Folder (${i})`)) {
       count++
     }
@@ -14,6 +12,6 @@ const newFolderName = function (folders) {
   return `New Folder (${count + 1})`
 }
 
-console.log(newFolderName(["New Folder", "New Folder (2)", "New Folder (3)"]))
+console.log(newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)']))
 
 module.exports = { newFolderName }
