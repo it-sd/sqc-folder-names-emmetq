82498431f97223a33e6a8d88a77a40603f819ae8
Emmet Quint <emmetmquint@gmail.com>
2023-03-08 21:33:40 -0600

    Implemented function and tests

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const newFolderName = function (folders) {
+  let count = 1
+  
+  if (!folders.includes('New Folder')) {
+      return 'New Folder'
+    } 
 
+  for (i = 0; i < folders.length + 1; i++) {
+    if (folders.includes(`New Folder (${i})`)) {
+      count++
+    }
+  }
+
+  return `New Folder (${count + 1})`
 }
+
+console.log(newFolderName(["New Folder", "New Folder (2)", "New Folder (3)"]))
+
+module.exports = { newFolderName }
